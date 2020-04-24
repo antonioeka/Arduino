5647509088b0023e970e813e03879ba101006de5
antonioeka <64261853+antonioeka@users.noreply.github.com>
2020-04-24 19:35:09 +0700

    Add files via upload

---
+++ Tugas2.ino
@@ -0,0 +1,15 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+void setup() {
+  lcd.init();
+
+}
+
+void loop() {
+ lcd.setCursor(0,0);
+ lcd.print("Work From Home !!");
+ lcd.setCursor(0,1);
+ lcd.print("I'm Happy ");
+
+}
